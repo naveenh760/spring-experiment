697d58a20fae0dfa648f2458fabb3293b117c022
Naveen <naveenh@ninjacart.com>
2022-09-27 21:12:45 +0530

    initial commit

---
+++ src/main/java/com/example/springexperiment/SpringExperimentApplication.java
@@ -0,0 +1,14 @@
+package com.example.springexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExperimentApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
